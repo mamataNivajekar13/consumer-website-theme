0a3f3717671c76cf897dcdc9a32c959b1221b8b1
Mamata Nivajekar <mamata.nivajekar@turtlemint.com>
2024-06-20 00:45:00 +0530

    bug fixes

--- turtlemint-child/functions.php
+++ turtlemint-child/functions.php
@@ -3,7 +3,7 @@
 add_action( 'wp_enqueue_scripts', 'tm_child_styles' );
 
 function tm_child_styles() {
-	wp_enqueue_style( 'tm-child-style', get_stylesheet_uri(), [], 1.2);
+	wp_enqueue_style( 'tm-child-style', get_stylesheet_uri('style.css'), [], 1.2);
 }
 
 ?>
